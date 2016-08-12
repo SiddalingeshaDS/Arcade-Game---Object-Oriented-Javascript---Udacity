f6cf11c440cf45a1bdd8bdd6367f8ab729c5ec99
Siddalingesha D S <siddhudev813@gmail.com>
2016-08-12 13:19:43 +0530

    Working Game - Initial

--- frontend-nanodegree-arcade-game/js/app.js
+++ frontend-nanodegree-arcade-game/js/app.js
@@ -2,6 +2,11 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    var _xOffset = 101;
+    var _yOffset = 60;
+    this.randomSpeedRatio = 50 + Math.floor(100*Math.random(Date.now));
+    this.x = _xOffset;
+    this.y = _yOffset + ( 83 * (Math.floor(3 * Math.random(Date.now()))));
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,6 +19,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if((this.x + (this.randomSpeedRatio * dt)) < 505){
+        this.x += (this.randomSpeedRatio * dt);
+    }else{
+        this.x = 0;
+    }
+    this.render();
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,12 +36,77 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(){
+    
+    this.initialPositions = {'x' : 200, 'y' : 400};
+    this.offsets = {
+        'left' : {'x' : -101, 'y' : 0},
+        'up' : {'x' : 0, 'y' : -83},
+        'right' : {'x' : 101, 'y' : 0},
+        'down' : {'x' : 0, 'y' : 83},
+    };
+    this.limits = {
+        'leftX' : -83,
+        'rightX' : 422,
+        'topY' : 0,
+        'bottomY' : 400
+    }
+    this.x = this.initialPositions.x;
+    this.y = this.initialPositions.y;
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(){
+    self = this;
+    allEnemies.forEach(function(enemy) {
+        if((((enemy.x + 80 >= self.x) && (enemy.x < self.x + 80)) ) && ((self.y >= enemy.y) && (self.y - enemy.y < 63))){
+            self.reset();
+        }
+    });
+
+};
+
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyCode){
+    if(['left','right','up','down'].indexOf(keyCode) == -1)
+        return;
+    if(this.checkLimits(keyCode)){
+        this.x += this.offsets[keyCode].x;
+        this.y += this.offsets[keyCode].y;
+    }else{
+        this.reset();
+    }
+    this.render();
+};
+
+Player.prototype.checkLimits = function(keyCode){
+    var _tempX = this.x + this.offsets[keyCode].x;
+    var _tempY = this.y + this.offsets[keyCode].y;
+    if(_tempX < this.limits.leftX || _tempX > this.limits.rightX || _tempY < this.limits.topY || _tempY > this.limits.bottomY)
+        return false;
+    return true;
+};
+
+
+Player.prototype.reset = function(){
+    this.x = this.initialPositions.x;
+    this.y = this.initialPositions.y;
+    this.render();
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [];
+for(var i = 0 ; i < 4 ; i++){
+    allEnemies.push(new Enemy());
+}
 
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -41,6 +117,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });

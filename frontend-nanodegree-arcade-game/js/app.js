de3dfd3426615cc8e87eb39c63c32caf5c641487
Siddalingesha D S <siddhudev813@gmail.com>
2016-08-12 13:55:15 +0530

    Refactor and Adding comments

--- frontend-nanodegree-arcade-game/js/app.js
+++ frontend-nanodegree-arcade-game/js/app.js
@@ -4,18 +4,22 @@
 * @constructor
 */
 var Enemy = function() {
-    // Initial variables to hold the size and step of the enemy positions
-    var _xOffset = -101;
-    var _yOffset = 60;
     // width of the canvas used to restart the enemy flow
     this.widthOfCanvas = 505;
     // used to set the speed of the enemy
     var minSpeed = 100;
     var speedVariation = 300;
     this.randomSpeedRatio =  minSpeed + Math.floor(speedVariation * Math.random(Date.now));
+    
+    // Initial variables to hold the size and step of the enemy positions
+    var _xOffset = -101;
+    var _yOffset = 60;
+    var _rowHeight = 83; // rowHeight of the blocks where the enemies can be placed
+    
     // Random initialization of the y value to start anywhere on the three block paths
     this.x = _xOffset;
-    this.y = _yOffset + ( 83 * (Math.floor(3 * Math.random(Date.now()))));
+    this.y = _yOffset + ( _rowHeight * (Math.floor(3 * Math.random(Date.now()))));
+
     this.sprite = 'images/enemy-bug.png';
 };
 

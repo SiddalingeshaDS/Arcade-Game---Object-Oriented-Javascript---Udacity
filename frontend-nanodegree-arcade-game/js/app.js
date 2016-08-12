7d35d711a810e8090ddeb81fd3fac068319b0510
Siddalingesha D S <siddhudev813@gmail.com>
2016-08-12 13:46:49 +0530

    Adding Commnents

--- frontend-nanodegree-arcade-game/js/app.js
+++ frontend-nanodegree-arcade-game/js/app.js
@@ -36,9 +36,8 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+//---------------------------------------------------
+
 /**
 * @description Represents a Player
 * @constructor
@@ -122,9 +121,9 @@ Player.prototype.reset = function(){
     this.render();
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Instantiating the objects.
+// All enemy objects are in an array called allEnemies
+// The player object in a variable called player
 
 var allEnemies = [];
 // Number of enemies : 4
@@ -136,7 +135,7 @@ for(var i = 0 ; i < numberOfEnemies; i++){
 var player = new Player();
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
